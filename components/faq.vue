cc1b20f915daf7e469aa0ee6e96a90d249411120
Leszek <leszek@strixspace.com>
2023-11-15 17:28:56 +0100

    🚧 Working on FAQu

---
+++ components/faq.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="mt-20">
+    <div class="grid grid-cols-12 ">
+      <div class="col-span-12 lg:col-start-4 lg:col-span-6">
+
+        <div v-for="(item, index) in faqItems" :key="index">
+          <h5 class="block antialiased tracking-normal font-sans text-xl leading-snug text-inherit mt-6 mb-1 font-semibold !text-black">
+            {{ item.question }}
+          </h5>
+          <div class="block antialiased font-sans text-base leading-relaxed mb-4 font-normal text-gray-600">
+            {{ item.answer }}
+          </div>
+          <hr class="my-6 border-blue-gray-50">
+        </div>
+        
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+defineProps({
+  faqItems: Array
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -18,4 +18,15 @@
     title="Title"
     description="Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?" 
     actionUrl="#" />
+
+  <Faq :faqItems="[
+    { 
+      question: 'Question', 
+      answer: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.' 
+    },
+    {
+      question: 'Question',
+      answer: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?'
+    },
+  ]"/>
 </template>
