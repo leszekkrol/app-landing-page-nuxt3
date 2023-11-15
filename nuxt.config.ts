2ed08012ae90eb7cff492f3dddb7b9fdfd14de4e
Leszek <leszek@strixspace.com>
2023-11-15 12:57:00 +0100

    ⚒️ Setup Tailwind CSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  app: {
+    head: {
+      title: "App Landing Page in Nuxt3",
+    },
+  },
+  css: [
+    '~/assets/main.css',
+  ],
+  modules: [
+    '@nuxtjs/tailwindcss',
+  ],
 })
