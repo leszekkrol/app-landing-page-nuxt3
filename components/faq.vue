2a62d28a9e96ff2471b0579137630c69abc32811
Leszek W. Król <contact@leszekkrol.com>
2024-07-25 15:41:28 +0200

    ⚒️ Npm update

--- components/faq.vue
+++ components/faq.vue
@@ -2,7 +2,6 @@
   <section class="mt-20">
     <div class="grid grid-cols-12 ">
       <div class="col-span-12 lg:col-start-4 lg:col-span-6">
-
         <div v-for="(item, index) in faqItems" :key="index">
           <h5 class="block antialiased tracking-normal font-sans text-xl leading-snug text-inherit mt-6 mb-1 font-semibold !text-black">
             {{ item.question }}
@@ -12,7 +11,6 @@
           </div>
           <hr class="my-6 border-blue-gray-50">
         </div>
-        
       </div>
     </div>
   </section>
